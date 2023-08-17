e594fc17822d9df13f871534743bb372c991d692
avianurag721 <anurag.721721@gmail.com>
2023-08-17 10:15:52 +0530

    changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://rablobackend-d3o3.onrender.com",
     credentials: true,
   })
 );
